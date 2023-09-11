d84b73a39bd45fd8bd329dca4a45ec01010903c0
Abheek Tripathy <abheek.tripathy@gmail.com>
2023-09-11 14:00:54 +0530

    feat: add mdx files

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <h1>Avail Project Developer Docs</h1>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/availproject/availproject.github.io',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'this is good?, i think.',
   },
 }
 
